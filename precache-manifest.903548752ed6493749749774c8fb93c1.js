4ff8297280cf44dcd8ebfb1ab13f56520a251562
SashaZaicev <sasha167@bk.ru>
2020-04-06 22:25:44 +0300

    Updates

---
+++ precache-manifest.903548752ed6493749749774c8fb93c1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53c6b91d25679a57e82257b4671ea9a9",
+    "url": "/to-do-list-zaicev/index.html"
+  },
+  {
+    "revision": "868e4973225de9c70f26",
+    "url": "/to-do-list-zaicev/static/css/main.d0571738.chunk.css"
+  },
+  {
+    "revision": "74546f3922413f799b0d",
+    "url": "/to-do-list-zaicev/static/js/2.00286792.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/to-do-list-zaicev/static/js/2.00286792.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "868e4973225de9c70f26",
+    "url": "/to-do-list-zaicev/static/js/main.ca02fb0a.chunk.js"
+  },
+  {
+    "revision": "a43c4b1000b5f6e66388",
+    "url": "/to-do-list-zaicev/static/js/runtime-main.8dee4a54.js"
+  },
+  {
+    "revision": "7d8fa4d6b838358db50c7d80d20b4670",
+    "url": "/to-do-list-zaicev/static/media/drew-beamer-unsplash.7d8fa4d6.jpg"
+  },
+  {
+    "revision": "93017a2449c59968ba4706794ce783eb",
+    "url": "/to-do-list-zaicev/static/media/leaf-3155438_192022.93017a24.jpg"
+  },
+  {
+    "revision": "ead6fa3251a7dcf38ca52675f1cea68c",
+    "url": "/to-do-list-zaicev/static/media/paper-1990111_1920.ead6fa32.jpg"
+  }
+]);
